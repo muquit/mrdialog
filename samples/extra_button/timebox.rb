d212b657d3f2a06b36d67c26d3dc596001aae3c8
Human Capitalist <human-capitalist@allaiz.com>
2021-07-18 09:02:23 -0400

    read dialog output when using extra button with: inputbox, menu, password, radiolist, timebox

---
+++ samples/extra_button/timebox.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+# muquit@muquit.com Apr-01-2014 
+require [File.expand_path(File.dirname(__FILE__)), '../..', 'lib', 'mrdialog'].join('/')
+require 'pp'
+
+begin
+  ME = File.basename($0)
+  if ENV['CHANGE_TITLE']
+    if ME =~ /(.+)\.rb$/
+      base = $1
+      puts "\033]0;mrdialog - #{base}\007"
+    end
+  end
+
+  text = <<EOF
+Please set the time...
+EOF
+
+  h = 0
+  w = 0
+  t = Time.new
+
+  dialog = MRDialog.new
+  dialog.logger = Logger.new(ENV["HOME"] + "/dialog_" + ME + ".log")
+  dialog.clear = true
+  dialog.title = "TIEMBOX"
+  dialog.extra_button = true
+  dialog.ok_label = "Event"
+  dialog.extra_label = "Alert"
+  dialog.cancel_label = "Quit"
+
+  # return Time
+  time = dialog.timebox(text, h, w, t)
+
+  puts "Exit Code: #{dialog.exit_code}"
+  puts "time: #{time}"
+
+rescue => e
+  puts "#{$!}"
+  t = e.backtrace.join("\n\t")
+  puts "Error: #{t}"
+end
